a77742d55fd09b5cc599244ad2dc59e4a16c5564
adityakurnias <adityakurniasaputra903@gmail.com>
2025-04-08 22:33:30 +0700

    PLEASE READ BEFORE WORKING: Changing 'soal-siswa' page to Dynamic Page and move it to '/soal/' folder, if you want to see the page go to '/soal/{anything}' url ex: '/soal/1' and add 'MultipleChoice' component for question handling

---
+++ components/MultipleChoice.vue
@@ -0,0 +1,21 @@
+<template>
+  <URadioGroup :ui="{
+    base: 'ring-primary-light/25 cursor-pointer',
+    item: 'flex items-center',
+    indicator: 'bg-primary-light',
+    label: 'text-primary-light text-lg cursor-pointer',
+  }" :items="items" />
+</template>
+
+<script lang="ts" setup>
+import type { RadioGroupItem, RadioGroupValue } from "@nuxt/ui";
+
+defineProps({
+  items: {
+    type: Array as () => RadioGroupItem[],
+    required: true,
+  },
+});
+</script>
+
+<style></style>
